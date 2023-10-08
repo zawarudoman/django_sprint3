a43345b1171e414dd4e8071a62c7410512c2114f
Максим Лесков <max.leskoff@icloud.com>
2023-10-08 15:06:18 +0300

    4 commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from blog.models import Post, Category
+from blog.models import Category, Post
 
 NUMBER_OF_POSTS_VISIBLE = 5
 
@@ -33,11 +33,11 @@ def post_detail(request, post_id):
 
 def category_posts(request, category_slug):
     """Page output category_posts"""
-    posts = get_request().filter(category__slug=category_slug)
-    context = {"post_list": posts,
-               'category': get_object_or_404(
-                   Category,
-                   slug=category_slug,
-                   is_published=True
-               )}
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    posts = get_request().filter(category=category)
+    context = {"post_list": posts, }
     return render(request, "blog/category.html", context)
